80332e51886145ee6744ebf9f8f06e20cacbba64
cixinii@163.com <cixinii@163.com>
2017-08-01 21:57:58 +0800

    实现注册功能

---
+++ static/js/jianshu.js
@@ -0,0 +1,11 @@
+ var name,telephone,passward;
+ name=getElementsByClassName('name').innerHTML;
+ telephone=getElementsByClassName('telephone').innerHTML;
+ passward=getElementsByClassName('password').innerHTML
+function myFunction{
+	if (name=null) {
+		alert("请输入昵称");
+	}
+	alert("你好")
+	
+}
