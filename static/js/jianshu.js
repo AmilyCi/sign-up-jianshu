d90492d8dc995fd2a91ea34a7b8a877aa29404e2
cixinii@163.com <cixinii@163.com>
2017-08-16 23:05:48 +0800

    添加提示功能

--- static/js/jianshu.js
+++ static/js/jianshu.js
@@ -1,11 +1,25 @@
-var Dom_name = document.getElemenstByClassName('name');
-var Dom_telephone = document.getElemenstByClassName('telephone');
-var Dom_password = document.getElemenstByClassName('password');
+var Dom_name = document.getElementsByClassName('name');
+var Dom_telephone = document.getElementsByClassName('telephone');
+var Dom_password = document.getElementsByClassName('password');
 function submitFunction(){
 	var name =Dom_name[0].value;
 	var telephone = Dom_telephone[0].value;
 	var password = Dom_password[0].value;
-	if (name=="") {
-		
+	if (name=='') {
+		document.getElementsByClassName('alarm-name')[0].style.display='block';
 	}
-}
+		else if (name!='') {
+			document.getElementsByClassName('alarm-name')[0].style.display='none';
+			if (telephone=='') {
+			document.getElementsByClassName('alarm-telephone')[0].style.display='block';
+			}
+				else if (telephone!='') {
+					document.getElementsByClassName('alarm-telephone')[0].style.display='none';
+					if (password=='') {
+						document.getElementsByClassName('alarm-passward')[0].style.display='block';
+						else if (password!='') {
+							document.getElementsByClassName('alarm-passward')[0].style.display='none';
+						}
+						}
+					}				
+		}
