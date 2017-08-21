765d8ad08a2e8db173096f1a5bc728e54c855a55
cixinii@163.com <cixinii@163.com>
2017-08-21 22:35:19 +0800

    完善注册页面功能

--- static/js/jianshu.js
+++ static/js/jianshu.js
@@ -1,25 +1,45 @@
+
 var Dom_name = document.getElementsByClassName('name');
 var Dom_telephone = document.getElementsByClassName('telephone');
 var Dom_password = document.getElementsByClassName('password');
+var Dom_addtelephone=document.getElementsByClassName('addtelephone')
 function submitFunction(){
-	var name =Dom_name[0].value;
+	var name = Dom_name[0].value;
 	var telephone = Dom_telephone[0].value;
 	var password = Dom_password[0].value;
-	if (name=='') {
+	if(name=='') {
 		document.getElementsByClassName('alarm-name')[0].style.display='block';
+		return;
 	}
-		else if (name!='') {
-			document.getElementsByClassName('alarm-name')[0].style.display='none';
-			if (telephone=='') {
-			document.getElementsByClassName('alarm-telephone')[0].style.display='block';
-			}
-				else if (telephone!='') {
-					document.getElementsByClassName('alarm-telephone')[0].style.display='none';
-					if (password=='') {
-						document.getElementsByClassName('alarm-passward')[0].style.display='block';
-						else if (password!='') {
-							document.getElementsByClassName('alarm-passward')[0].style.display='none';
-						}
-						}
-					}				
-		}
+	else{
+		document.getElementsByClassName('alarm-name')[0].style.display='none';
+		
+	}
+	if (telephone=='') {
+		document.getElementsByClassName('alarm-telephone')[0].style.display='block';
+		return;
+	}
+	else{
+		document.getElementsByClassName('alarm-telephone')[0].style.display='none';
+
+	}
+	if (password=='') {
+		document.getElementsByClassName('alarm-passward')[0].style.display='block';
+		return;
+	}
+	else{
+		document.getElementsByClassName('alarm-passward')[0].style.display='none';
+	}
+}
+function myFunction(x){
+	if (telephone!='') {
+		document.getElementsByClassName('yzm')[0].style.display='block';
+	}
+	else{
+		document.getElementsByClassName('yzm')[0].style.display='none';
+	}
+}
+
+
+
+
